a28e3c5be1bec56eb326ce8a844636933b9638a5
86150 <Alfredzhanglp@163.com>
2020-07-03 21:06:59 +0800

    增加功能

---
+++ src/com/itfei/HelloWorld/Hello_World.java
@@ -0,0 +1,13 @@
+package com.itfei.HelloWorld;
+
+/**
+ * @Description : 测试修改clone的代码 提交github
+ * @Author : Alfred
+ * @Date : 2020/7/3 21:01
+ **/
+
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
