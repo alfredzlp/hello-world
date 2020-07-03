f2c5295d62c929ef57080cd124e18c69274cf9e8
unknown <alfredzhanglp@163.com>
2020-07-03 20:51:47 +0800

    first commit

---
+++ src/com/itfei/demo/Hello_GitHub.java
@@ -0,0 +1,13 @@
+package com.itfei.demo;
+
+/**
+ * @Description : 上传github
+ * @Author : Alfred
+ * @Date : 2020/7/3 20:34
+ **/
+
+public class Hello_GitHub {
+    public static void main(String[] args) {
+        System.out.println("welcome to github!");
+    }
+}
